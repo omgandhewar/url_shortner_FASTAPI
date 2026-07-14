9c5c92c19ae777085af6dba0b554db7e3f1f9a27
omgandhewar <gandhewarom@gmail.com>
2026-07-15 01:13:00 +0530

    Update script file

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,9 @@
 function login(successcallback){
 
-    let email=document.querySelector("email").value;
-    let password=document.querySelector("password").value;
+    let email=document.querySelector("#email").value;
+    let password=document.querySelector("#password").value;
 
-    fetch("/http://127.0.0.1:8000/login",{
+    fetch("http://127.0.0.1:8000/login",{
         method:"POST",
         headers:{
             "Content-Type":"application/json"
@@ -21,4 +21,17 @@ function login(successcallback){
         console.log(data);
         successcallback();
     })
+    
+}
+
+let loginform=document.querySelector("#loginform");
+if(loginform){
+    loginform.addEventListener("submit",function(event){
+
+        event.preventDefault();
+
+        login(function(){
+            window.location.href="dashboard.html";
+        });
+    });
 }
