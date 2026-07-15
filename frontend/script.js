415b38436773532c0c7ea1a7eb07508bab60ea29
omgandhewar <gandhewarom@gmail.com>
2026-07-15 19:32:53 +0530

    Update frontend script and signup page

--- frontend/script.js
+++ frontend/script.js
@@ -35,3 +35,45 @@ if(loginform){
         });
     });
 }
+
+function signup(signupcallback){
+
+    let name=document.querySelector("#username").value;
+    let email=document.querySelector("#email").value;
+    let password=document.querySelector("#password").value;
+
+    fetch("/http://127.0.0.1:8000/signup",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            name:name,
+            email:email,
+            password:password
+        })
+    })
+    .then(function(response){
+
+        if(!response.ok){
+            throw error("invalid credential");
+        }
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        signupcallback();
+    })
+}
+
+let signupform=document.querySelector("#signupform");
+if(signupform){
+    signupform.addEventListener("submit",function(event){
+
+        event.preventDefault();
+
+        signup(function(){
+            window.location.href="login.html";
+        })
+    })
+}
